cfaa6ba7a3f49348d6d0fc5904d5a6c3785a1506
antogp <88955960+antogp24@users.noreply.github.com>
2024-02-17 17:14:20 -0500

    Update init.el

--- init.el
+++ init.el
@@ -3,18 +3,3 @@
   "Emacs.org"
   user-emacs-directory))
 
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(visual-fill-column org-bullets rainbow-delimiters evil-magit magit counsel-projectile helpful projectile doom-modeline ivy which-key doom-themes tree-sitter-langs tree-sitter evil)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
